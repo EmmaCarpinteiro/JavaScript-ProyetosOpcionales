adf8954f0c52f5f1d5b3607e0137b66907364e11
Emma <emma.carpinteiro@softwareone.com>
2022-07-29 10:26:20 +0200

    JS Dog Years añadido

---
+++ dogYears.js
@@ -0,0 +1,30 @@
+let myAge = 23;
+//Edad actual
+
+let earlyYears = 2;
+//Variable con nombre
+
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+//Resultado menos 2
+
+laterYears *= 4;
+// Multiplicamos la variable por 4
+
+console.log(earlyYears);
+console.log(laterYears);
+
+let myAgeInDogYears = earlyYears + laterYears;
+console.log(myAgeInDogYears);
+//Sumando variables
+
+let myName = "Emma";
+console.log(myName.toLowerCase());
+//Devolver nombre
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years wich is ${myAgeInDogYears} years old in dog years.`);
+//Devolvemos por consola nuestras variables creadas
+
+myAge = 9;
+console.log(myAge);
