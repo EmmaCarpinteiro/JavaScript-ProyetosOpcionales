294f9b55459cca6db2b6b942409b16ff6739b623
Emma <emma.carpinteiro@softwareone.com>
2022-07-29 10:27:34 +0200

    JS Kelvin Weather añadido

---
+++ kelvinWeather.js
@@ -0,0 +1,20 @@
+const kelvin = 293;
+console.log(kelvin);
+// Creamos una constante llamada kelvin 
+
+let celsius = kelvin - 20;
+console.log(celsius);
+// Creamos variable Celsius 
+
+let fahrenheit = celsius * (9/5) + 32;
+console.log(fahrenheit);
+//Calculamos los Fahrenheit
+
+console.log(Math.floor(fahrenheit));
+//Visualizacion
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+
+Newton = celsius * (33/100);
+console.log(Math.floor(Newton));
+console.log(Newton);
