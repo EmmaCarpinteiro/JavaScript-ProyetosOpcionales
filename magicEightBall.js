a2c09cbcb417efaccd1411da885bed4012dcc74c
EmmaCarpinteiro <104421560+EmmaCarpinteiro@users.noreply.github.com>
2022-08-15 15:44:43 +0200

    Update magicEightBall.js

--- magicEightBall.js
+++ magicEightBall.js
@@ -13,29 +13,29 @@ let eightBall = "";
 
 switch (randomNumber) {
   case 0:
-  eightBall = "It is certain";
-  break;
+    eightBall = "It is certain";
+    break;
   case 1:
-  eightBall = "It is decidely so";
-  break;
+    eightBall = "It is decidely so";
+    break;
    case 2:
-  eightBall = "Reply hazy try again";
-  break;
+    eightBall = "Reply hazy try again";
+    break;
    case 3:
-  eightBall = "Can not predict now";
-  break;
+    eightBall = "Can not predict now";
+    break;
    case 4:
-  eightBall = "Do not count on it";
-  break;
+    eightBall = "Do not count on it";
+    break;
    case 5:
-  eightBall = "My sources say no";
-  break;
+    eightBall = "My sources say no";
+    break;
    case 6:
-  eightBall = "Outlook not so good";
-  break;
+    eightBall = "Outlook not so good";
+    break;
    case 7:
-  eightBall = "Signs point to yes";
-  break;
+    eightBall = "Signs point to yes";
+    break;
 }
 
 console.log(`The Magic 8 Ball says, ${eightBall}.`);
